b84e43be3207a20b1b17d83c98d2ec988bb4e3a5
red <72036824+redx1109@users.noreply.github.com>
2026-08-03 23:37:14 +0530

    Update food.js

--- snake2.0/food.js
+++ snake2.0/food.js
@@ -6,7 +6,7 @@ function s2SpawnBigFood() {
         tooClose = s2Foods.some(f => Math.hypot(f.x - x, f.y - y) < 60)
             || s2BigFoods.some(f => Math.hypot(f.x - x, f.y - y) < 60);
     } while (tooClose);
-    s2BigFoods.push({ x, y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)` });
+    s2BigFoods.push({ x, y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)`, ox: x, oy: y, orbitA: Math.random()*Math.PI*2 });
 }
 
 function s2InitBigFood() {
@@ -22,7 +22,7 @@ function s2SpawnFood() {
         tooClose = s2Foods.some(f => Math.hypot(f.x - x, f.y - y) < 50)
             || s2BigFoods.some(f => Math.hypot(f.x - x, f.y - y) < 50);
     } while (tooClose);
-    s2Foods.push({ x, y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)` });
+    s2Foods.push({ x, y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)`, ox: x, oy: y, orbitA: Math.random()*Math.PI*2 });
 }
 
 function s2InitFood() {
@@ -38,9 +38,9 @@ function s2DropFoodTrail(snakeArr) {
             if (s2Foods.length + s2BigFoods.length >= S2_MAX_FOOD && s2Foods.length > 0) {
                 s2Foods.shift(); // evict oldest normal food to make room
             }
-            if (s2Foods.length + s2BigFoods.length < S2_MAX_FOOD) s2BigFoods.push({ x: snakeArr[i].x, y: snakeArr[i].y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)` });
+            if (s2Foods.length + s2BigFoods.length < S2_MAX_FOOD) s2BigFoods.push({ x: snakeArr[i].x, y: snakeArr[i].y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)`, ox: snakeArr[i].x, oy: snakeArr[i].y, orbitA: Math.random()*Math.PI*2 });
         } else {
-            if (s2Foods.length + s2BigFoods.length < S2_MAX_FOOD) s2Foods.push({ x: snakeArr[i].x, y: snakeArr[i].y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)` });
+            if (s2Foods.length + s2BigFoods.length < S2_MAX_FOOD) s2Foods.push({ x: snakeArr[i].x, y: snakeArr[i].y, color: `hsl(${Math.floor(Math.random()*360)},90%,60%)`, ox: snakeArr[i].x, oy: snakeArr[i].y, orbitA: Math.random()*Math.PI*2 });
         }
     }
 }
